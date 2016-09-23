bb8c3ceea982c0e75107399a06d584a5e7be2f71
jcp510 <jcponce510@gmail.com>
2016-09-23 16:05:08 -0700

    Move all images to src_images/, configure responsive_images task in Gruntfile.js.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,23 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-
+    responsive_images: {
+      dev: {
+        options: {
+          // Task-specific options go here.
+          engine: 'im'
+        },
+        files: [{
+          // Target-specific file lists and/or options go here.
+          expand: true,
+          src: ['*.{png,jpg}'],
+          cwd: 'src_images/',
+          dest: 'images'
+        }]
+      }
+    }
   });
+  grunt.loadNpmTasks('grunt-responsive-images');
+  // Default task(s).
+  grunt.registerTask('default', ['responsive_images']);
 };
